aeee36b38b883d84c8c607b40e654e88c345b15e
DTom <>
2023-10-19 18:31:55 +0700

    ve hinh tam giac

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from in_so import InSo
 from ve_hcn import hcn
 from ve_hv import hv
+from ve_tamgiac import tamgiac
 print('hello')
 InSo()
 print()
 hcn(4)
 print()
 hv(4)
+print()
+tamgiac(4)
---
+++ ve_tamgiac.py
@@ -0,0 +1,4 @@
+def tamgiac(n):
+    a = 0
+    for b  in range(1, n+1):
+        print('*  ' * b)
